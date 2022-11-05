74223e082225aae56a03e17ff440b907e75c123d
henryzord <henry.cagnini@gmail.com>
2022-11-05 08:27:21 -0300

    adapted code to read sql script and generate db from it

--- app/static/database/__init__.py
+++ app/static/database/__init__.py
@@ -6,92 +6,7 @@ import os
 import sqlite3
 
 
-def cria_tabelas(cur: sqlite3.Cursor) -> sqlite3.Cursor:
-    """
-    Cria as tabelas do banco de dados.
-
-    :param cur: um cursor para o banco de dados.
-    :return: um cursor para o banco de dados.
-    """
-
-    cur.execute('''
-        CREATE TABLE IF NOT EXISTS materias (
-            id_materia INTEGER NOT NULL,
-            nome TEXT NOT NULL,
-            periodos INTEGER NOT NULL,
-            PRIMARY KEY (id_materia)
-        )
-    ''')
-
-    cur.execute('''
-        CREATE TABLE IF NOT EXISTS professores (
-            id_professor INTEGER NOT NULL,
-            nome TEXT NOT NULL,
-            cabelo TEXT NOT NULL,
-            barba TEXT NOT NULL,
-            PRIMARY KEY (id_professor)
-        )
-    ''')
-
-    cur.execute('''
-        CREATE TABLE IF NOT EXISTS professores_para_materias (
-            id_professor INTEGER NOT NULL,
-            id_materia INTEGER NOT NULL,
-            data_inicio TEXT NOT NULL DEFAULT '2022-04-11',
-            data_fim TEXT NOT NULL DEFAULT '2023-02-15',
-            PRIMARY KEY (id_professor, id_materia, data_inicio, data_fim),
-            FOREIGN KEY (id_professor) REFERENCES professores(id_professor),
-            FOREIGN KEY (id_materia) REFERENCES materias(id_materia)
-        )
-    ''')
-
-    return cur
-
-
-def insere_tuplas(cur: sqlite3.Cursor) -> sqlite3.Cursor:
-    """
-    Insere tuplas no banco de dados.
-
-    :param cur: um cursor para o banco de dados.
-    :return: um cursor para o banco de dados.
-    """
-
-    cur.execute('''INSERT INTO professores(id_professor, nome, cabelo, barba) VALUES (1, 'Fábio', 'não', 'não');''')
-    cur.execute('''INSERT INTO professores(id_professor, nome, cabelo, barba) VALUES (2, 'Henry', 'sim', 'sim');''')
-    cur.execute('''INSERT INTO professores(id_professor, nome, cabelo, barba) VALUES (3, 'Rafael Pereira', 'sim', 'sim');''')
-    cur.execute('''INSERT INTO professores(id_professor, nome, cabelo, barba) VALUES (4, 'Lairane', 'sim', 'não');''')
-    cur.execute('''INSERT INTO professores(id_professor, nome, cabelo, barba) VALUES (5, 'Mário', 'sim', 'não');''')
-    cur.execute('''INSERT INTO professores(id_professor, nome, cabelo, barba) VALUES (6, 'Gustavo', 'sim', 'sim');''')
-    cur.execute('''INSERT INTO professores(id_professor, nome, cabelo, barba) VALUES (7, 'Karina', 'sim', 'não');''')
-    cur.execute('''INSERT INTO professores(id_professor, nome, cabelo, barba) VALUES (8, 'Roberto', 'sim', 'não');''')
-    cur.execute('''INSERT INTO professores(id_professor, nome, cabelo, barba) VALUES (9, 'Priscila', 'sim', 'não');''')
-    cur.execute('''INSERT INTO professores(id_professor, nome, cabelo, barba) VALUES (10, 'Shirley', 'sim', 'não');''')
-
-    cur.execute('''INSERT INTO materias(id_materia, nome, periodos) VALUES (1, 'Internet das Coisas', 3);''')
-    cur.execute('''INSERT INTO materias(id_materia, nome, periodos) VALUES (2, 'Banco de Dados', 3);''')
-    cur.execute('''INSERT INTO materias(id_materia, nome, periodos) VALUES (3, 'Desenvolvimento de Sistemas', 3);''')
-    cur.execute('''INSERT INTO materias(id_materia, nome, periodos) VALUES (4, 'Sociologia', 1)''')
-    cur.execute('''INSERT INTO materias(id_materia, nome, periodos) VALUES (5, 'Física', 3)''')
-    cur.execute('''INSERT INTO materias(id_materia, nome, periodos) VALUES (6, 'Princípios de Gestão', 3)''')
-    cur.execute('''INSERT INTO materias(id_materia, nome, periodos) VALUES (7, 'História', 2)''')
-
-    cur.execute('''INSERT INTO professores_para_materias(id_professor, id_materia) VALUES (1, 1);''')
-    cur.execute('''INSERT INTO professores_para_materias(id_professor, id_materia) VALUES (1, 2);''')
-    cur.execute('''INSERT INTO professores_para_materias(id_professor, id_materia) VALUES (2, 3);''')
-
-    cur.execute('''INSERT INTO professores_para_materias(id_professor, id_materia, data_inicio, data_fim) VALUES (4, 5, '2022-04-11', '2022-05-11');''')
-    cur.execute('''INSERT INTO professores_para_materias(id_professor, id_materia, data_inicio, data_fim) VALUES (5, 5, '2022-05-11', '2022-06-11');''')
-    cur.execute('''INSERT INTO professores_para_materias(id_professor, id_materia, data_inicio, data_fim) VALUES (6, 5, '2022-06-11', '2023-02-15');''')
-
-    cur.execute('''INSERT INTO professores_para_materias(id_professor, id_materia, data_inicio, data_fim) VALUES (7, 6, '2022-04-11', '2022-05-11');''')
-    cur.execute('''INSERT INTO professores_para_materias(id_professor, id_materia, data_inicio, data_fim) VALUES (8, 6, '2022-05-11', '2022-06-11');''')
-    cur.execute('''INSERT INTO professores_para_materias(id_professor, id_materia, data_inicio, data_fim) VALUES (9, 6, '2022-06-11', '2023-02-15');''')
-    cur.execute('''INSERT INTO professores_para_materias(id_professor, id_materia, data_inicio, data_fim) VALUES (10, 6, '2022-06-11', '2023-02-15');''')
-
-    return cur
-
-
-def main(database_path: str = None):
+def main(database_path: str = None, script_sql_path: str = None):
     print('---------- rotina do banco de dados ----------')
 
     # pega o caminho absoluto do arquivo do banco de dados
@@ -99,6 +14,13 @@ def main(database_path: str = None):
     # database_path vai ser C:\Users\aluno\test.db
     if database_path is None:
         database_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test.db')
+
+    if script_sql_path is None:
+        script_sql_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'script.sql')
+
+        if not os.path.exists(script_sql_path):
+            raise FileNotFoundError('Precisa haver um arquivo script.sql na pasta app/static/database, '
+                                    'com as rotinas de criação de tabelas e inserção de tuplas!')
 
     # se o banco de dados existir, deleta-o
     # isso facilita a vida na hora que formos re-gerar o banco, pois evita que tuplas
@@ -116,9 +38,11 @@ def main(database_path: str = None):
         # um cursor é uma conexão para o banco de dados (e.g. cria, deleta, insere, etc)
         cur = con.cursor()
 
-        cur = cria_tabelas(cur)
+        with open(script_sql_path, 'r') as script_sql:
+            lines = '\n'.join(script_sql.readlines())
+            cur.executescript(lines)
+
         print('tabelas criadas!')
-        cur = insere_tuplas(cur)
         print('tuplas inseridas!')
 
         # salva as modificações feitas no banco
